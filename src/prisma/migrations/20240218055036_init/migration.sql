6be99195f03857cd69fc6ba216ff6acd56a9531f
tianopo <matheuslink18@hotmail.com>
2024-02-18 02:50:52 -0300

    consertado em inglês

--- src/prisma/migrations/20240218055036_init/migration.sql
+++ src/prisma/migrations/20240218055036_init/migration.sql
@@ -3,9 +3,9 @@ CREATE TABLE `user` (
     `id` VARCHAR(191) NOT NULL,
     `criadoEm` DATETIME(3) NOT NULL,
     `atualizadoEm` DATETIME(3) NOT NULL,
-    `nome` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
     `email` VARCHAR(191) NOT NULL,
-    `senha` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
 
     UNIQUE INDEX `user_id_key`(`id`),
     UNIQUE INDEX `user_email_key`(`email`),
