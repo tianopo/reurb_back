d52d4dfb2b55271dc4f736b52218d7ef3262c44d
tianopo <matheuslink18@hotmail.com>
2024-02-28 14:03:05 -0300

    adicionando tipagem

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./modules/app.module";
+import { NestExpressApplication } from "@nestjs/platform-express";
 
 const bootstrap = async () => {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   const cors = {
     origin: ['http://localhost:3000'],
