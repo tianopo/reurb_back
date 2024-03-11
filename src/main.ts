278b052559b2cf656dd9d41f4acaf74a06eb2ca4
tianopo <matheuslink18@hotmail.com>
2024-03-11 17:39:44 -0300

    salvando token que dura 24 horas

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ const bootstrap = async () => {
   app.useGlobalPipes(new CustomValidationPipe());
 
   const cors = {
-    origin: ["http://localhost:9000"],
+    origin: ["http://localhost:8999"],
     methods: "GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS",
   };
 
